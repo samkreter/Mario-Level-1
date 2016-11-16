7df090c7be3d4eb6d869848d1269353a11cf6d79
Sam Kreter <samkreter@gmail.com>
2016-11-16 10:54:05 -0600

    removed music from the game

--- data/game_sound.py
+++ data/game_sound.py
@@ -23,8 +23,8 @@ class Sound(object):
             # pg.mixer.music.play()
             self.state = c.NORMAL
         elif self.overhead_info.state == c.GAME_OVER:
-            pg.mixer.music.load(self.music_dict['game_over'])
-            pg.mixer.music.play()
+            # pg.mixer.music.load(self.music_dict['game_over'])
+            # pg.mixer.music.play()
             self.state = c.GAME_OVER
 
 
@@ -38,47 +38,47 @@ class Sound(object):
         """Handles the state of the soundn object"""
         if self.state == c.NORMAL:
             if self.mario.dead:
-                self.play_music('death', c.MARIO_DEAD)
+                # self.play_music('death', c.MARIO_DEAD)
             elif self.mario.invincible \
                     and self.mario.losing_invincibility == False:
-                self.play_music('invincible', c.MARIO_INVINCIBLE)
+                # self.play_music('invincible', c.MARIO_INVINCIBLE)
             elif self.mario.state == c.FLAGPOLE:
-                self.play_music('flagpole', c.FLAGPOLE)
+                # self.play_music('flagpole', c.FLAGPOLE)
             elif self.overhead_info.time == 100:
-                self.play_music('out_of_time', c.TIME_WARNING)
+                # self.play_music('out_of_time', c.TIME_WARNING)
 
 
         elif self.state == c.FLAGPOLE:
             if self.mario.state == c.WALKING_TO_CASTLE:
-                self.play_music('stage_clear', c.STAGE_CLEAR)
+                # self.play_music('stage_clear', c.STAGE_CLEAR)
 
         elif self.state == c.STAGE_CLEAR:
             if self.mario.in_castle:
-                self.sfx_dict['count_down'].play()
+                # self.sfx_dict['count_down'].play()
                 self.state = c.FAST_COUNT_DOWN
 
         elif self.state == c.FAST_COUNT_DOWN:
             if self.overhead_info.time == 0:
-                self.sfx_dict['count_down'].stop()
+                # self.sfx_dict['count_down'].stop()
                 self.state = c.WORLD_CLEAR
 
         elif self.state == c. TIME_WARNING:
             if pg.mixer.music.get_busy() == 0:
-                self.play_music('main_theme_sped_up', c.SPED_UP_NORMAL)
+                # self.play_music('main_theme_sped_up', c.SPED_UP_NORMAL)
             elif self.mario.dead:
-                self.play_music('death', c.MARIO_DEAD)
+                # self.play_music('death', c.MARIO_DEAD)
 
         elif self.state == c.SPED_UP_NORMAL:
             if self.mario.dead:
-                self.play_music('death', c.MARIO_DEAD)
+                # self.play_music('death', c.MARIO_DEAD)
             elif self.mario.state == c.FLAGPOLE:
-                self.play_music('flagpole', c.FLAGPOLE)
+                # self.play_music('flagpole', c.FLAGPOLE)
 
         elif self.state == c.MARIO_INVINCIBLE:
             if (self.mario.current_time - self.mario.invincible_start_timer) > 11000:
-                self.play_music('main_theme', c.NORMAL)
+                # self.play_music('main_theme', c.NORMAL)
             elif self.mario.dead:
-                self.play_music('death', c.MARIO_DEAD)
+                # self.play_music('death', c.MARIO_DEAD)
 
 
         elif self.state == c.WORLD_CLEAR:
@@ -90,8 +90,8 @@ class Sound(object):
 
     def play_music(self, key, state):
         """Plays new music"""
-        pg.mixer.music.load(self.music_dict[key])
-        pg.mixer.music.play()
+        # pg.mixer.music.load(self.music_dict[key])
+        # pg.mixer.music.play()
         self.state = state
 
     def stop_music(self):
