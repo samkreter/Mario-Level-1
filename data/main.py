28fcb6afcbfa9dd9e14f93f936902ffd24c722aa
Sam Kreter <samkreter@gmail.com>
2016-12-05 10:53:12 -0600

    added selection operator

--- data/main.py
+++ data/main.py
@@ -29,31 +29,49 @@ def main():
     childFitness = []
     while(1):
 
+        ####Generation
         for i in range(NumParents):
 
             #Mutate the parents
             child = list(parents[i])
             # for index in (lastPos + random.sample(range(len(MOVES)), NumMutations)):
             #     child[index] = random.choice(moveList)
-            while(1):
-                tmp = list(child)
 
+
+            while(1):
+                #Randomize around point of death to find living solution
                 if(lastPos):
                     for index in range(lastPos - 200,lastPos + 10):
                         child[index] = random.choice(moveList)
 
+                #Run the fitness game
                 run_it = tools.Control(child)
                 state_dict = {c.LEVEL1: level1.Level1()}
                 run_it.setup_states(state_dict, c.LEVEL1)
                 cRun = run_it.main()
+
+                #Get pos where death occured
                 lastPos = cRun['counter']
 
                 if not cRun['mario dead']:
-                    print("We Lived")
-                    break;
+                    print("Produced Working Solution")
+                    break
 
+            #add solution and fitness
+            children.append(child)
+            childFitness.append(cRun['current time'])
 
-#44 + 77 + 48 + 60 / 3
+        #Combine parents and child for selection (U+V)
+        parents = parents + children
+        parentsFitness = parentsFitness + childFitness
+
+        #Selection############
+        selectSort = [i for i in sorted(enumerate(parentsFitness), key=lambda x:x[1])]
+        selected = zip(*selectSort)[0][0:NumParents]
+
+        #replace old parents with the newly selected ones
+        parents = [parents[i] for i in selected]
+        parentsFitness = [parentsFitness[i] for i in selected]
 
 
 
